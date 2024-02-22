f1eba3677fa68c13ca0ea3b7b6934a9b9cb94d5f
imadLAKHLIFI <imadlakhlifi13@gmail.com>
2024-02-22 02:09:30 +0100

    show Loading Skeletons

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import ApiClient from "../Services/Api-client";
 import { CanceledError } from "axios";
-export interface Platform{
+export interface Platform {
   id: number;
-  name:string;
-  slug:string;
+  name: string;
+  slug: string;
 }
 export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform:Platform}[];
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
 
@@ -22,17 +22,20 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     ApiClient.get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {setGames(res.data.results);setLoading(false)})
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
     return () => controller.abort();
   }, []);
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,15 +1,22 @@
 import { SimpleGrid, Text, layout } from "@chakra-ui/react";
 import useGames from "../Hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const Skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} padding="10px" spacing={10}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
+        {isLoading && Skeletons.map(skeleton=> <GameCardSkeleton key={skeleton}/>)}
         {games.map((game) => (
-          <GameCard key={game.id} game={game}/>
+          <GameCard key={game.id} game={game} />
         ))}
       </SimpleGrid>
     </>

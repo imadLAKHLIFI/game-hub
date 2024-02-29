3466400a485798da7ac2e4f83bde586d0f47f0fd
imadLAKHLIFI <imadlakhlifi13@gmail.com>
2024-02-29 21:07:14 +0100

    Fetch the Genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 function App() {
   return (
     <Grid templateAreas={{
@@ -12,7 +13,7 @@ function App() {
       </GridItem>
      <Show above="lg">
       <GridItem area={"aside"}>
-        Aside
+        <GenreList/>
       </GridItem>
       </Show>
       <GridItem area={"main"} >
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,15 +1,15 @@
-import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
-import React from 'react'
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import React from "react";
 
 const GameCardSkeleton = () => {
   return (
     <Card>
-        <Skeleton height={'200px'}/>
-        <CardBody>
-            <SkeletonText/>
-        </CardBody>
+      <Skeleton height={"200px"} />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCardSkeleton
+export default GameCardSkeleton;

de27faa06d33871f3ec8fed59e42c96ade2ab2e1
imadLAKHLIFI <imadlakhlifi13@gmail.com>
2024-03-31 01:12:40 +0100

    Update page Title

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
-import SearchInput from './searchinput'
+import SearchInput from './SearchInput'
 interface Props{
   onSearch: (searchText:string)=> (void);
 }
